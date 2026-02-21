c92ff038ce0cf0ba58858266e5ab84d6af052cca
Cecília <cecbrennand@gmail.com>
2026-02-21 19:28:45 -0300

    aprendendo sobre listas

---
+++ capitulo6/listas.py
@@ -0,0 +1,22 @@
+# Uma maneira de definir listas
+
+idades = [15, 50, 38, 28, 25, 18, 44]
+
+print('Soma das idades:', sum(idades))
+
+print('Quantidade de itens:', len(idades))
+
+print('Média das idades:', sum(idades)/len(idades))
+
+print('Menor idade:', min(idades))
+
+print('Maior idade:', max(idades))
+
+cec = ['cecília', 26, 'publicitária', 'recife', 1999, ['Kovu', 'Annie', 'Nami']]
+print(cec[5][1])
+
+tamanho = len(cec)
+ultimo = len(cec) - 1
+cachorros = cec[ultimo]
+ultimo_cachorro = len(cachorros) - 1
+print(cachorros[ultimo_cachorro])

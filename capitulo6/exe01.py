9e67fd21e67373c14af482dde541d595978e4ef2
Cecília <cecbrennand@gmail.com>
2026-02-21 20:39:57 -0300

    fazendo mais exercícios

--- capitulo6/exe01.py
+++ capitulo6/exe01.py
@@ -1,8 +1,18 @@
 lista = []
+cont = 0
 
-for i in range(0, 4):
-   numero = input('Escreva um número: ')
-   lista += numero
+while True:
+   numero = input('Digite um número:')
+   if numero == '':
+      break
+   else:
+      num = int(numero)
+      lista.append(num)
 
-num_esc = input('Escolha um número dos que você escreveu: ')
+num_esc = int(input('Agora escolha um número dos que você digitou: '))
+for i in (lista):
+   if i == num_esc:
+      cont += 1
 
+print(lista)
+print('O número', num_esc, 'apareceu', cont, 'vezes na lista.')

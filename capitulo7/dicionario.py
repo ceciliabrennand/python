1a7a4fee99b51a55f7761fc934f54154961d6478
Cecília <cecbrennand@gmail.com>
2026-02-22 19:14:22 -0300

    aprendendo mais sobre dicionários

--- capitulo7/dicionario.py
+++ capitulo7/dicionario.py
@@ -1,3 +1,4 @@
+# %%
 dados_cec = {
     'nome':'cecília',
     'sobrenome':'brennand',
@@ -15,3 +16,20 @@ dados_cec = {
 print(dados_cec['idade'])
 print(dados_cec['cachorros'][-1])
 print(dados_cec['cargos'][-1]['empresa'])
+
+dados_cec['série'] = 'arcane'
+print(dados_cec)
+print('Chaves:', dados_cec.keys())
+print('Valores:', dados_cec.values())
+print('Itens:', dados_cec.items())
+
+for i in dados_cec:
+    print(i, '-->', dados_cec[i])
+
+for i in dados_cec.items():
+    print(i)
+
+for chave, valor in dados_cec.items():
+    print(chave, '-->', valor)
+
+    

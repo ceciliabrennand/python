b3b0add025057035edb143689db46f5cecfe67c7
Cecília <cecbrennand@gmail.com>
2026-02-22 19:29:37 -0300

    aprendendo sobre tuplas

---
+++ capitulo7/tuplas.py
@@ -0,0 +1,11 @@
+
+dados_cec = [26, 'brennand', 'publicitária', 'dev']
+
+dados_cec.append(33)
+print(dados_cec)
+
+tupla_cec = (26, 'brennand', 'publicitária', 'dev', ['kovu', 'annie'])
+print(tupla_cec)
+
+tupla_cec[-1].append('nami')
+print(tupla_cec)

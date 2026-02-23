b7f7af1be911f9d666a535cf02f8b81e73368834
Cecília <cecbrennand@gmail.com>
2026-02-22 21:33:54 -0300

    corrigi um erro

--- capitulo7/exe03_2.py
+++ capitulo7/exe03_2.py
@@ -4,11 +4,10 @@ while True:
     frase = input('Digite uma frase: ')
     if frase == '':
         break
-
-if frase not in dados:
-    dados[frase] = 1
-else:
-    dados[frase] += 1
+    if frase not in dados:
+        dados[frase] = 1
+    else:
+        dados[frase] += 1
 
 # Se a frase não estiver em frases, então o valor é 1. Senão, é adicionado mais 1 ao valor. 
 

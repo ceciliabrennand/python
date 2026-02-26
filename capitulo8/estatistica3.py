d36467ddbc52dd00d1a135ac50e7bdcf6782eea6
Cecília <cecbrennand@gmail.com>
2026-02-26 19:44:58 -0300

    aprendendo sobre kwargs

--- capitulo8/estatistica3.py
+++ capitulo8/estatistica3.py
@@ -1,13 +1,14 @@
 # Média
 
-def soma(a:float, b:float, *args):
+def soma(a:float, b:float, *args)->float:
     valores = [a, b] + list(args)
     return sum(valores)
 
-def media(a:float, b:float, c=0)->float:
-    return soma(a, b) / 2
+def media(a:float, b:float, *args)->float:
+    return soma(a, b, *args) / (len(args) + 2)
 
-x = float(input('Digite um número: '))
-y = float(input('Digite outro número: '))
+a = float(input('Digite um número: '))
+b = float(input('Digite outro número: '))
+c = float(input('Digite outro número: '))
 
-print('Média:', media(x, y))
+print('Média:', media(a, b, c))
---
+++ capitulo8/precos.py
@@ -0,0 +1,10 @@
+def calc_imposto(preco:float, tx_base:float, **kwargs):
+    imposto = preco * tx_base
+
+    for i in kwargs:
+        print(i, kwargs[i])
+        imposto += preco * kwargs[i]
+    
+    return imposto
+
+print(calc_imposto(100, 0.03, municipio=0.01, estadual=0.005, nacional=0.001))

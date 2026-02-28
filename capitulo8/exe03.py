a4589ab8f320f2142f18483207cec3079601fa39
Cecília <cecbrennand@gmail.com>
2026-02-27 22:33:56 -0300

    exercitando funções

---
+++ capitulo8/exe03.py
@@ -0,0 +1,12 @@
+# Função simples
+
+def saudacao():
+    print('Bem-vindo!')
+
+saudacao()
+
+def saudar():
+    nome = input('Qual o seu nome? ')
+    print('Olá,', nome, '!')
+
+saudar()
---
+++ capitulo8/exe04.py
@@ -0,0 +1,11 @@
+# Função com retorno
+
+def soma(a, b):
+    return a + b
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+total = soma(n1, n2)
+
+print(n1, '+', n2, '=', total)

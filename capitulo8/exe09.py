6fc34ad99a0bfc30bc2e52e3394281ada38950fa
Cecília <cecbrennand@gmail.com>
2026-02-28 11:48:52 -0300

    exercitando funções

--- capitulo8/exe09.py
+++ capitulo8/exe09.py
@@ -1,10 +1,14 @@
-def soma(a, b, *numeros)
-    total = a + b + list(numeros)
-    return 
-
-def media(a, b, *numeros):
-    total = a + b + lista / len(lista) + 2
+def soma(*numeros):
+    total = sum(numeros)
     return total
+    
+def media(*numeros):
+    if len(numeros) == 0 or len(numeros) == 1:
+        erro = 'Não podemos fazer a média sem no mínimo 2 números.'
+        return erro
+    else:
+        m = soma(*numeros) / len(numeros)
+        return m
 
 lista = []
 
@@ -16,4 +20,30 @@ while True:
         numero = int(num)
         lista.append(numero)
 
-media(n1, n2, lista)
+print(media(*lista))
+
+# %% código mais limpo 
+
+def soma(*numeros):
+    total = sum(numeros)
+    return total
+    
+def media(*numeros):
+    if len(numeros) < 2:
+        erro = 'Não podemos fazer a média sem no mínimo 2 números.'
+        return erro
+    
+    m = soma(*numeros) / len(numeros)
+    return m
+
+lista = []
+
+while True:
+    num = input('Digite um número:')
+    if num == '':
+        break
+    else:
+        numero = int(num)
+        lista.append(numero)
+
+print(media(*lista))

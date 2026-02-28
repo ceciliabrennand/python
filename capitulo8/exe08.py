75aea85ab9dee3faef147ceb493a02d7d172d5f7
Cecília <cecbrennand@gmail.com>
2026-02-27 23:24:54 -0300

    exercitando funções

---
+++ capitulo8/exe08.py
@@ -0,0 +1,8 @@
+def calculos(a, b):
+    soma = a + b
+    subtracao = a - b
+    multi = a * b
+    divisao = a / b
+    return soma, subtracao, multi, divisao
+
+print(calculos(3, 5))
---
+++ capitulo8/exe09.py
@@ -0,0 +1,19 @@
+def soma(a, b, *numeros)
+    total = a + b + list(numeros)
+    return 
+
+def media(a, b, *numeros):
+    total = a + b + lista / len(lista) + 2
+    return total
+
+lista = []
+
+while True:
+    num = input('Digite um número:')
+    if num == '':
+        break
+    else:
+        numero = int(num)
+        lista.append(numero)
+
+media(n1, n2, lista)

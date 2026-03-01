d789b48d617b0943edc4185afca3e79d1048ca89
Cecília <cecbrennand@gmail.com>
2026-03-01 17:46:54 -0300

    aprendendo sobre módulos

---
+++ capitulo9/modulos.py
@@ -0,0 +1,16 @@
+import math as mh
+from math import pi, e
+
+print(mh.sqrt(9))
+
+print(pi, e)
+
+import random
+print(random.randint(1, 10))
+
+# %%
+import random
+
+x = ['teo', 'cec', 'tham', 'arthu', 'josh']
+
+random.choice(x)

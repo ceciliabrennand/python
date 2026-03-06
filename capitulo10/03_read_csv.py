a5c1830deab81211f66d1bf562da8179dec25f11
Cecília <cecbrennand@gmail.com>
2026-03-05 22:57:45 -0300

    aprendendo sobre arquivos

--- capitulo10/03_read_csv.py
+++ capitulo10/03_read_csv.py
@@ -3,6 +3,9 @@ arquivo= "C:/Users/cisab/Documents/python/capitulo10/data.csv"
 open_file = open(arquivo)
 
 with open(arquivo, "r", encoding="utf-8") as open_file:
-    data = open_file.read()
+    data = open_file.readlines()
 
 print(data)
+
+for linha in data:
+    print(linha)

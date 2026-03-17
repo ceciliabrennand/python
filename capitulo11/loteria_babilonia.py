dec7a88a2544c82cc9092bde737881ce4872f89f
Cecília <cecbrennand@gmail.com>
2026-03-17 20:39:15 -0300

    deixando o código melhor de ser lido

--- capitulo11/loteria_babilonia.py
+++ capitulo11/loteria_babilonia.py
@@ -11,6 +11,17 @@ def get_input():
         if 1 <= num_escolhido <= 100:
             return num_escolhido
         print('Número invalido. Por favor, escolha um número de 1 a 100.')    
+
+def check_numbers(num_sorteado, num_escolhido):
+    if num_escolhido > num_sorteado:
+        print('Errou. O número sorteado é menor.')
+        return False
+    elif num_escolhido < num_sorteado:
+        print('Errou. O número sorteado é maior.')
+        return False
+    else:
+        print('Parabéns! Você acertou o número sorteado.')
+        return True
    
 num_sorteado = random.randint(1, 100)
 
@@ -19,13 +30,7 @@ print('Você tem 3 tentativas para acertar o número sorteado.')
 
 for i in range(1, 4):
     num_escolhido = get_input()
-        
-    if num_escolhido > num_sorteado:
-        print('Errou. O número sorteado é menor.')
-    elif num_escolhido < num_sorteado:
-        print('Errou. O número sorteado é maior.')
-    else:
-        print('Parabéns! Você acertou o número sorteado.')
+    if check_numbers(num_sorteado=num_sorteado, num_escolhido=num_escolhido):
         break
 
 else:

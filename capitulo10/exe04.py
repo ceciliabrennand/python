cbfeb9b53ab4dc37f1fe82ac5ed600e404d69fd6
Cecília <cecbrennand@gmail.com>
2026-03-21 11:19:43 -0300

    exercitando arquivos

--- capitulo10/exe04.py
+++ capitulo10/exe04.py
@@ -1,9 +1,14 @@
 with open('capitulo10/mensagem.txt', 'r', encoding='utf-8') as arquivo:
     conteudo = arquivo.readlines()
     print(conteudo)
+    tamanho = len(conteudo)
 
 # Conteúdo é uma lista, então precisa adicionar uma nova linha à uma lista
-for i in range(len(conteudo)):
+for i in range(tamanho): #Tem que botar range porque tamanho é um número
     conteudo[i] = conteudo[i].strip()
+
+print(conteudo)
+
+conteudo.append('Linha 5')
 
 print(conteudo)
---
+++ capitulo10/exe04_2.py
@@ -0,0 +1,2 @@
+with open('capitulo10/mensagem.txt', 'a', encoding='utf-8') as arquivo:
+    novo_conteudo = arquivo.write('\nLinha 5')

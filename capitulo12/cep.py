7eaf0cd2e0e00b32470e9323208086e308110341
Cecília <cecbrennand@gmail.com>
2026-04-11 15:55:01 -0300

    aprendendo api

--- capitulo12/cep.py
+++ capitulo12/cep.py
@@ -2,6 +2,10 @@
 
 import requests # para realizar requisições na web
 import json # para tratar json de listas/dicionarios para arquivos json
+from tqdm import tqdm
+
+import pandas as pd
+
 ceps = [
     "50010010",
     "50010050",
@@ -13,12 +17,20 @@ ceps = [
 url = "https://viacep.com.br/ws/{cep}/json/"
 
 dados = []
-for i in ceps:
+
+for i in tqdm(ceps):
     resposta = requests.get(url.format(cep=i))
     if resposta.status_code == 200:
         dados.append(resposta.json())
 
 print(dados)
+
+# %%
+
+dataset = pd.DataFrame(dados)
+dataset
+
+# %%
 
 with open('capitulo12/ceps.json', 'w', encoding='utf-8') as arquivo:
     json.dump(dados, arquivo, ensure_ascii=False, indent=4)
